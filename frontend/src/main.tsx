565229a5e9e32f74e31bb8739c8b63eb5d8e1f7c
Titya Yem <thitya.yem.photo@gmail.com>
2025-06-10 00:27:13 -0400

    installed frontend with bun

---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import { Theme } from '@radix-ui/themes'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Theme>
+      <App />
+    </Theme>
+  </StrictMode>,
+)
